f2bcbf722cbbe85036d978921060106cd495b48e
pratikkad10 <148075940+pratikkad10@users.noreply.github.com>
2023-10-29 15:08:59 +0530

    Create dictionary_datatype.py

---
+++ dictionary_datatype.py
@@ -0,0 +1,24 @@
+#DICTIONARY DATA
+
+dic1={
+    "Name" : "jayesh",
+    "Rollno" : "2015",
+    "Branch" : "mechanical"
+}
+
+dic2={
+    "Name" : "Atharva",
+    "Rollno" : "2007",
+    "Branch" : "Computer"
+}
+
+dic3={
+    "Name" : "Anuj",
+    "Rollno" : "2015",
+    "Branch" : "Information Technology"
+}
+print(dic1["Branch"], dic2["Branch"])
+
+
+X = dic1.keys()
+print(X)
