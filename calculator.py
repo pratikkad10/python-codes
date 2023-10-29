7842525c43afaa77171d6c06bcfb16fcad13be06
pratikkad10 <148075940+pratikkad10@users.noreply.github.com>
2023-10-29 12:51:28 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,13 @@
+#HERE IS AN SIMPLE CALCULATOR
+
+a=int(input("Enter first value "))
+b=int(input("Enter second value "))
+
+c=a+b
+print("The value of a + b = ", c)
+d=a-b
+print("The value of a - b = ", d)
+e=a*b
+print("The value of a * b = ", e)
+f=a/b
+print("The value of a / b = ", f)
