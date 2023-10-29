755fd044e92e0c3fc04432ec43569725315ec169
pratikkad10 <148075940+pratikkad10@users.noreply.github.com>
2023-10-29 15:25:34 +0530

    Create fancy_format.py

---
+++ fancy_format.py
@@ -0,0 +1,9 @@
+#FANCY OR FORMAT
+
+
+Fname=input("Enter first name ")
+Mname=input("Enter middle name ")
+Lname=input("Enter last name ")
+age= input("Enter age ")
+address=input("Enter address ")
+print(f" first name is {Fname}\n middle name is {Mname}\n last name is {Lname}\n age is {age}\n address is {address}5")
