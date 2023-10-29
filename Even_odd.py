9bb73f9536e14c98b219e249bc2261f63ef01992
pratikkad10 <148075940+pratikkad10@users.noreply.github.com>
2023-10-29 15:16:51 +0530

    Create Even_odd.py

---
+++ Even_odd.py
@@ -0,0 +1,11 @@
+#even odd numbers between 1 to 1000
+
+for i in range (1, 1001):
+    if i%2==0:
+
+        print(i, "EVEN")
+        i+=1
+        
+    if i%2==1:
+        print (i, "ODD")
+        i+=1
