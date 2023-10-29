21790f7fc0b5f9292b90402225041c37c25e3641
pratikkad10 <148075940+pratikkad10@users.noreply.github.com>
2023-10-29 12:52:42 +0530

    Create basicprogrammes.py

---
+++ basicprogrammes.py
@@ -0,0 +1,14 @@
+#To grade students on basis of marks
+
+marks=int(input("Enter your marks "))
+
+if marks>=90:
+    print("A+")
+elif marks>=80:
+    print("A")
+elif marks>=70:
+    print("B+")
+elif marks>=55:
+    print("B")
+else:
+    print("C")
